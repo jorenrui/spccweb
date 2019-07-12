8a92a12cf0d248eaf265630f2d6e1dbe3ac2b647
Joeylene Rivera <joeylenerivera@gmail.com>
2019-07-12 16:55:45 +0800

    Add Mobile Navigation - Add an external javascript file - Move particle.js config to external js file - Add hamburger menu

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+function toggleNavResponsive() {
+    let siteNav = document.getElementById('siteNav');
+
+    if (siteNav.className === 'site-nav') {
+        siteNav.className += ' responsive';
+    } else {
+        siteNav.className = 'site-nav';
+    }
+
+    let navIcon = document.getElementById('navIcon');
+
+    if (navIcon.className === 'fa fa-2x fa-bars') {
+        navIcon.className = ' fa fa-2x fa-times';
+    } else {
+        navIcon.className = 'fa fa-2x fa-bars';
+    }
+}
+
+(() => {
+    'use strict';
+
+    // Particles.js - Site header bg config
+    let particles = Particles.init({
+        selector: '.particles-background',
+        color: '#f2f2f2',
+        connectParticles: true,
+        maxParticles: 100,
+        responsive: [{
+                breakpoint: 768,
+                options: {
+                    maxParticles: 50
+                }
+            },
+            {
+                breakpoint: 425,
+                options: {
+                    maxParticles: 30,
+                    connectParticles: false
+                }
+            },
+            {
+                breakpoint: 320,
+                options: {
+                    maxParticles: 0
+                }
+            }
+        ]
+    });
+})();
