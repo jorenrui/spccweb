46c490a672216c2261ff09ea1ae0a5f3ffcfb5ae
Joeylene Rivera <joeylenerivera@gmail.com>
2019-08-09 19:06:36 +0800

    Add Alert Messages

--- resources/views/layouts/headers/plain.blade.php
+++ resources/views/layouts/headers/plain.blade.php
@@ -1,2 +1,11 @@
 <div class="header bg-gradient-primary pb-5 pt-5 pt-md-8">
+    <div class="container-fluid">
+        <div class="header-body">
+            <div class="row">
+              <div class="col">
+                @include('layouts.headers.messages')
+              </div>
+            </div>
+        </div>
+    </div>
 </div>
