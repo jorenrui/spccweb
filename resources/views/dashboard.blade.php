f2cb3b2d5bb2060b07bc6426d0f9702e4cd9406a
Joeylene Rivera <joeylenerivera@gmail.com>
2019-08-09 12:34:18 +0800

    Remove unneeded scripts

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -108,8 +108,3 @@
         @include('layouts.footers.auth')
     </div>
 @endsection
-
-@push('js')
-    <script src="{{ asset('argon') }}/vendor/chart.js/dist/Chart.min.js"></script>
-    <script src="{{ asset('argon') }}/vendor/chart.js/dist/Chart.extension.js"></script>
-@endpush
