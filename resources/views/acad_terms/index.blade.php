b587893d857893d74f09e3e04fc2df01677dccb2
Joeylene Rivera <joeylenerivera@gmail.com>
2019-08-27 17:58:32 +0800

    Updated card header

--- resources/views/acad_terms/index.blade.php
+++ resources/views/acad_terms/index.blade.php
@@ -13,7 +13,7 @@
                 <div class="card-header border-0">
                     <div class="row align-items-center">
                         <div class="col">
-                            <h3 class="mb-0">Academic Terms</h3>
+                            <h3 class="mb-0">Academic Term Masterlist</h3>
                         </div>
                         <div class="col text-right">
                             <a href="/acad_terms/create" class="btn btn-sm btn-primary">Add Academic Term</a>
