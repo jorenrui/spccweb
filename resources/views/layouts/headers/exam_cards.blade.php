c863ebc4b3e03a9e4bd8dc698db7cbe5ee318b72
Joeylene Rivera <joeylenerivera@gmail.com>
2019-09-03 22:30:31 +0800

    Add alert messages

--- resources/views/layouts/headers/exam_cards.blade.php
+++ resources/views/layouts/headers/exam_cards.blade.php
@@ -1,6 +1,7 @@
 <div class="header bg-gradient-primary pb-8 pt-5 pt-md-8">
   <div class="container-fluid">
       <div class="header-body">
+        @include('layouts.headers.messages')
           <!-- Card stats -->
           <div class="row">
               <div class="col-xl-3 col-sm-6">
