84b9d681cae8af52a9f57a6b7506963a65f82e7b
Joeylene Rivera <joeylenerivera@gmail.com>
2019-09-07 10:11:19 +0800

    Add Class Pages

---
+++ app/Http/Controllers/SClassController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SClass;
+use App\Models\Setting;
+
+use Illuminate\Http\Request;
+
+class SClassController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $classes = SClass::orderBy('class_id', 'asc')->paginate(15);
+        $degree = Setting::where('name', 'LIKE', 'Degree')->get()[0]->value;
+
+        return view('classes.index')
+                ->with('classes', $classes)
+                ->with('degree', $degree);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/classes/index.blade.php
@@ -0,0 +1,98 @@
+@extends('layouts.app', ['title' => 'Class Scheduling'])
+
+@section('content')
+    @include('layouts.headers.plain')
+
+    <div class="container-fluid mt--7">
+
+      <div class="row mt-5">
+        <div class="col-xl-12 mb-5 mb-xl-0">
+            <div class="card shadow">
+
+              @if(count($classes) > 0)
+                <div class="card-header border-0">
+                    <div class="row align-items-center">
+                        <div class="col">
+                            <h3 class="mb-0">Class Masterlist</h3>
+                            <p class="text-muted text-sm">{{ $degree }}</p>
+                        </div>
+                        <div class="col text-right">
+                            <a href="/classes/create" class="btn btn-sm btn-primary">Add Class</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="table-responsive">
+                    <table class="table align-items-center table-flush">
+                        <thead class="thead-light">
+                            <tr>
+                                <th scope="col"></th>
+                                <th scope="col" class="text-center">Course Code</th>
+                                <th scope="col" class="text-center">Description</th>
+                                <th scope="col" class="text-center">Credits</th>
+                                <th scope="col" class="text-center">Section</th>
+                                <th scope="col" class="text-center">Schedule</th>
+                                <th scope="col" class="text-center">Total Students Enrolled</th>
+                                @role('admin')
+                                <th scope="col"></th>
+                                @endrole
+                            </tr>
+                        </thead>
+                        <tbody>
+                          @foreach ($classes as $sclass)
+                            <tr>
+                              <td class="text-left" scope="row">
+                                  <a href="/classes/{{ $sclass->class_id }}/show" class="btn btn-outline-info btn-sm">
+                                      View
+                                  </a>
+                              </td>
+                              <td class="text-center">{{ $sclass->course->course_code }}</td>
+                              <td>{{ $sclass->course->description }}</td>
+                              <td class="text-center">{{ $sclass->course->units }}</td>
+                              <td class="text-center">{{ $sclass->section }}</td>
+                              <td class="text-center">{{ $sclass->day }}</td>
+                              <td class="text-center">-</td>
+                              @role('admin')
+                              <td class="text-right">
+                                <div class="dropdown">
+                                  <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                      <i class="fas fa-ellipsis-v"></i>
+                                  </a>
+                                  <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                                    <a href="/classes/{{ $sclass->class_id }}/edit" class="dropdown-item"">
+                                        Edit
+                                    </a>
+
+                                    <form method="POST" action="{{ action('SClassController@destroy', $sclass->class_id) }}" style="display: inline;">
+                                        @csrf
+                                        @method('DELETE')
+
+                                        <button type="submit"  class="dropdown-item">Delete</button>
+                                    </form>
+                                  </div>
+                                </div>
+                                @endrole
+                              </td>
+                            </tr>
+                          @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                <div class="card-footer">
+                    {{ $classes->links() }}
+                </div>
+              @else
+                  <div class="row mt-3 mb-5">
+                      <div class="col text-center">
+                          <p class="lead">No Class found</p>
+                          <br>
+                          <a href="/classes/create" class="btn btn-primary btn-lg">Add Class</a>
+                      </div>
+                  </div>
+              @endif
+            </div>
+        </div>
+      </div>
+
+      @include('layouts.footers.auth')
+    </div>
+@endsection
