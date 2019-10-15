4e07031c09a6ec2721a20808c437d1d07892d0c3
Joeylene Rivera <joeylenerivera@gmail.com>
2019-10-15 21:57:46 +0800

    remove url force scheme for app in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,8 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(config('app.env') === 'production') {
-            \URL::forceScheme('https');
-        }
+        //
+    //     if(config('app.env') === 'production') {
+    //         \URL::forceScheme('https');
+    //     }
     }
 }
