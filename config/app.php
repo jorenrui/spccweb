992bc5ed17013822935a3ca9783c4208dee7f28e
Joeylene Rivera <joeylenerivera@gmail.com>
2019-10-16 13:42:48 +0800

    Remove Cloudinary API

--- config/app.php
+++ config/app.php
@@ -161,7 +161,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        JD\Cloudder\CloudderServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -226,8 +225,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Cloudder' => JD\Cloudder\Facades\Cloudder::class,
-
     ],
 
 ];
