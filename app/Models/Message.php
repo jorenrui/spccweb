50a8e0ab8ce81eb5602482529d614c6b88d60e8f
Joeylene Rivera <joeylenerivera@gmail.com>
2019-11-02 23:26:39 +0800

    Add Message Table

---
+++ app/Models/Message.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    protected $table = 'message';
+    public $primaryKey = 'message_id';
+    public $timestamps = true;
+}
---
+++ database/migrations/2019_11_02_224307_create_message_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('message', function (Blueprint $table) {
+            $table->bigIncrements('message_id');
+            $table->string('name');
+            $table->string('email')->nullable();
+            $table->string('subject');
+            $table->mediumText('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('message');
+    }
+}
