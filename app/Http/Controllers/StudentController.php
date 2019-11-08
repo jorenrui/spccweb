c4ae474740e76957ab57d5d3974ee1af38658172
Joeylene Rivera <joeylenerivera@gmail.com>
2019-11-08 21:22:14 +0800

    Minor Fix

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -144,7 +144,7 @@ class StudentController extends Controller
         $student->save();
 
         return redirect('/students/' . $user->id)
-                ->with('success', 'Student ' . $student->getStudentNo() . ' Created. Default username is the Student No. with no dashes while the default password is the user\'s birthdate');;
+                ->with('success', 'Student ' . $student->getStudentNo() . ' Created. Default username is the Student No. with no dashes while the default password is the user\'s birthdate');
     }
 
     private function getEnlistment($student_no)
@@ -592,7 +592,7 @@ class StudentController extends Controller
         $student->save();
 
         return redirect('/students/' . $user->id)
-                ->with('success', 'Student ' . $student->getStudentNo() . ' Updated.');;
+                ->with('success', 'Student ' . $student->getStudentNo() . ' Updated.');
     }
 
     /**
