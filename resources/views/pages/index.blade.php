95c3db8896cd9456527548c7e077b28e111ad1a8
Joeylene Rivera <joeylenerivera@gmail.com>
2019-12-29 15:30:45 +0800

    Update Hero and About Section

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -18,24 +18,24 @@
 <!-- Upcoming Events Section -->
 @if(count($events) > 0)
 <section class="section upcoming-events">
-    <div class="title">
-      <h2 class="headline">Upcoming Events</h2>
-      @if(count($events) > 4)
-      <div class="pagination">
-        <button class="btn btn-left">
-          <i class="fa fa-3x fa-angle-left"></i>
-        </button>
-        <button class="btn btn-right">
-          <i class="fa fa-3x fa-angle-right"></i>
-        </button>
-      </div>
-      @endif
+  <div class="title">
+    <h2 class="headline">Upcoming Events</h2>
+    @if(count($events) > 4)
+    <div class="pagination">
+      <button class="btn btn-left">
+        <i class="fa fa-3x fa-angle-left"></i>
+      </button>
+      <button class="btn btn-right">
+        <i class="fa fa-3x fa-angle-right"></i>
+      </button>
     </div>
+    @endif
+  </div>
   @foreach ($events as $event)
-    <div class="card">
-      <h3 class="date">{{ $event->getEventDate() }}</h3>
-      <p class="event">{{ $event->title }}</p>
-    </div>
+  <div class="card">
+    <h3 class="date">{{ $event->getEventDate() }}</h3>
+    <p class="event">{{ $event->title }}</p>
+  </div>
   @endforeach
 </section>
 @endif
@@ -43,29 +43,32 @@
 
 <!-- Annoucement Section -->
 <section class="section annoucement">
-@if($annoucement != null)
+  @if($annoucement != null)
   <div class="content">
     <h2 class="headline">Annoucement</h2>
     <p>
       {{ $annoucement }}
     </p>
   </div>
-@endif
+  @endif
 </section>
 <!-- end Annoucement Section -->
 
 <!-- About Section -->
 <section class="section about-brief">
+  <div class="image">
+    <img src="{{ asset('spccweb/img/spcnian.jpg') }}" alt="SPCC Caloocan Gymnasium" />
+  </div>
   <div class="content">
     <h2 class="headline">Systems Plus Computer College</h2>
     <h3 class="sub-headline">Caloocan Campus</h3>
     <p class="description">
-      Systems Plus Computer College (SPCC) is a private school founded in 1997. It is affiliated with Systems Plus Computer Foundation (SPCF) which was founded in 1985 at Balibago, Angeles City, Pampanga on June 27, 1985. With the growth of the school, campuses have been established in Cubao, Caloocan City, San Fernando, and in Miranda, Angeles City.
+      Systems Plus Computer College (SPCC) is a private school founded in 1997. It is affiliated with Systems Plus
+      Computer Foundation (SPCF) which was founded in 1985 at Balibago, Angeles City, Pampanga on June 27, 1985. With
+      the growth of the school, campuses have been established in Cubao, Caloocan City, San Fernando, and in Miranda,
+      Angeles City.
     </p>
     <a href="/about" class="link">Learn More</a>
-  </div>
-  <div class="image">
-    <img src="{{ asset('spccweb/img/spcnian.jpg') }}" alt="SPCC flag ceremony" />
   </div>
 
   <div class="bg">
@@ -119,21 +122,21 @@
   <h2 class="headline">Latest News</h2>
 
   @foreach ($posts as $post)
-    <article>
-      <img src="{{ asset('/storage/cover_images/' . $post->cover_image) }}" />
-      <h3>
-        <a href="/articles/{{ $post->post_id }}" class="title">
-          {{ $post->title }}
-        </a>
-      </h3>
-      <div class="meta">
-        <span class="publish-date">{{ $post->getDateCreated() }}</span>
-      </div>
-      <p class="article-content">
-        {!! str_limit(strip_tags($post->body), 135) !!}
-      </p>
-      <a href="/articles/{{ $post->post_id }}" class="link">Read More</a>
-    </article>
+  <article>
+    <img src="{{ asset('/storage/cover_images/' . $post->cover_image) }}" />
+    <h3>
+      <a href="/articles/{{ $post->post_id }}" class="title">
+        {{ $post->title }}
+      </a>
+    </h3>
+    <div class="meta">
+      <span class="publish-date">{{ $post->getDateCreated() }}</span>
+    </div>
+    <p class="article-content">
+      {!! str_limit(strip_tags($post->body), 135) !!}
+    </p>
+    <a href="/articles/{{ $post->post_id }}" class="link">Read More</a>
+  </article>
   @endforeach
 
   <div class="bg">
